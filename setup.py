57eccfec6339d123ef42bad43061f7071f2cfec3
George Theofilis <g.theofilis@clmsuk.com>
2018-09-25 14:57:43 +0300

    Init project

---
+++ setup.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'paho-mqtt',
+    'influxdb',
+    'cloudant',
+    'ibmiotf'
+]
+
+setup_requirements = [ ]
+
+test_requirements = [ ]
+
+setup(
+    author="George Theofilis",
+    author_email='g.theofilis@clmsuk.com',
+    classifiers=[
+        'License :: OSI Approved :: Eclipse Public License 1.0 (EPL-1.0)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    install_requires=requirements,
+    license="EPL-1.0",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='chariot_northbound_dispatcher',
+    name='chariot_northbound_dispatcher',
+    packages=find_packages(include=[
+        'chariot_northbound_dispatcher',
+        'chariot_northbound_dispatcher.*'
+    ]),
+    entry_points={
+          'console_scripts': [
+
+          ]
+    },
+    scripts=[],
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/theofilis/chariot_northbound_dispatcher',
+    version='0.1.0',
+    zip_safe=False,
+)
