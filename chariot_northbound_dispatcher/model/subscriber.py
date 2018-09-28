696c5ebf1972f462a3adc520d43ea6ea92f03fd9
George Theofilis <g.theofilis@clmsuk.com>
2018-09-28 14:45:49 +0300

    Serialize subscriber

--- chariot_northbound_dispatcher/model/subscriber.py
+++ chariot_northbound_dispatcher/model/subscriber.py
@@ -4,4 +4,10 @@
 class Subscriber(object):
     def __init__(self, subscriber_id):
         self.id = subscriber_id
-        self.sensors = []
+        self.sensors = set()
+
+    def dict(self):
+        return {
+            'id': self.id,
+            'sensors': list(self.sensors)
+        }
--- chariot_northbound_dispatcher/resources/subscriber.py
+++ chariot_northbound_dispatcher/resources/subscriber.py
@@ -9,23 +9,23 @@ class SubscriberResource(object):
             'urn:ngsi-ld:security': Subscriber('urn:ngsi-ld:security')
         }
 
-        self.subscribers['bms'].sensors = {
+        self.subscribers['urn:ngsi-ld:bms'].sensors = {
             'urn:ngsi-ld:5410ec4d1601_humidity',
             'urn:ngsi-ld:5410ec4d1601_temperature',
             'urn:ngsi-ld:5410ec4d1601_din01'
         }
 
-        self.subscribers['bms'].sensors = {
+        self.subscribers['urn:ngsi-ld:security'].sensors = {
             'urn:ngsi-ld:5410ec4d1601_din02',
             'urn:ngsi-ld:5410ec4d1601_din03',
             'urn:ngsi-ld:5410ec4d1601_din04'
         }
 
     def on_post(self, req, resp):
-        subscriber_id = req.get_json('urn:ngsi-ld:id')
-        sensor_id = req.get_json('urn:ngsi-ld:sensor_id')
+        subscriber_id = req.get_json('id')
+        sensor_id = req.get_json('sensor_id')
 
         rule = self.subscribers[subscriber_id]
-        rule.sensors.push(sensor_id)
+        rule.sensors.add(sensor_id)
 
-        resp.json = rule
+        resp.json = rule.dict()
