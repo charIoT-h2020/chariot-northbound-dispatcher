80ff6ee6b58163d56325fc0a20016bc7d3cd1928
George Theofilis <g.theofilis@clmsuk.com>
2019-03-28 14:12:07 +0000

    Use subscribe service

--- chariot_northbound_dispatcher/dispatcher/dispatcher.py
+++ chariot_northbound_dispatcher/dispatcher/dispatcher.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 import logging
+import requests
 
 from chariot_base.utilities import Traceable
 from chariot_base.model import Subscriber
@@ -12,13 +13,8 @@ class Dispatcher(Traceable):
         self.southbound = None
         self.northbound = None
 
-        self.subscribers = {
-            'BMS': Subscriber('BMS')
-        }
-
-        self.subscribers['BMS'].sensors = {
-            'device_52806c75c3fd_Sensor04'
-        }
+        self.session = requests.Session()
+        self.session.trust_env = False
 
     def inject(self, southbound, northbound):
         self.southbound = southbound
@@ -36,9 +32,20 @@ class Dispatcher(Traceable):
             logging.debug('Message %s from %s send to %s' % (value, sensor_id, destination))
             self.set_tag(span, 'destination', destination)
 
-            if self.subscribers[destination] is not None:
-                if sensor_id in self.subscribers[destination].sensors:
-                    self.northbound.publish('%s/%s' % (destination, sensor_id), value)
+            url = 'http://172.18.1.20:8031/subscriber/{destination}'.format(**{'destination': destination})
+            headers = self.inject_to_request_header(span, url)
+            self.set_tag(span, 'url', url)
+            logging.debug('Retrieving subscription for: %s/%s' % (url, headers))
+            result = self.session.get(url, headers=headers)
+
+            if result is not None:
+                info = result.json()
+                logging.debug('Retrieve subscription info: %s' % (info))
+
+                if sensor_id in info['sensors']:
+                    topic = '%s/%s' % (destination, sensor_id)
+                    logging.debug('Retrieve subscription info: %s' % (topic))
+                    self.northbound.publish(topic, value)
 
         except:
             raise
