dd339a7a6e4e29a8819150b96a949f5e6055464f
George Theofilis <g.theofilis@clmsuk.com>
2019-03-29 14:50:00 +0200

    Edit build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CONTAINER_IMAGE="registry.gitlab.com/chariot-h2020/chariot-northbound-dispatcher"
-CI_COMMIT_TAG="v0.3.0"
+CI_COMMIT_TAG="v0.4.0"
 
 docker build --cache-from $CONTAINER_IMAGE:latest --tag $CONTAINER_IMAGE:$CI_COMMIT_TAG --tag $CONTAINER_IMAGE:latest -f ./Dockerfile.dispatcher .
 docker build --cache-from $CONTAINER_IMAGE/api:latest --tag $CONTAINER_IMAGE/api:$CI_COMMIT_TAG --tag $CONTAINER_IMAGE/api:latest .
