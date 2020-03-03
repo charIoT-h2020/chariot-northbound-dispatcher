cd456245c3adc22b99367aac10b1ec5a7a4410a4
George Theofilis <theofilis.g@gmail.com>
2020-03-03 11:40:56 +0000

    Bump version: 0.6.4 → 0.11.0

--- chariot_northbound_dispatcher/__init__.py
+++ chariot_northbound_dispatcher/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.6.4'
+__version__ = '0.11.0'
 __service_name__ = f'northbound-http-service-{__version__}'
 __service_name_listener__ = f'northbound-listener-service-{__version__}'
